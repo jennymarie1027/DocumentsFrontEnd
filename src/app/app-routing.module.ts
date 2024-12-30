bc8c1157a869efe8713ddeafb27afb119b19c686
McGrath <jennifer.mcgrath@sportsengine.com>
2024-12-30 15:21:16 -0700

    inital outline

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomepageComponent } from './homepage/homepage.component';
+import { DocumentsComponent } from './documents/documents.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomepageComponent },
+  { path: 'documents', component: DocumentsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
